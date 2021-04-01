235614457f63ef3dbbc6bb29d53fd1041cb97f45
Aidan <walkeraidan94@gmail.com>
2021-04-01 14:04:58 +0100

    Updates

---
+++ precache-manifest.e6f8a1a7c159f32a61a1fba4387d2257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c199e42a094998507b1ade9d2e1755",
+    "url": "/react_redux_no_middleware-basic-todo-list/index.html"
+  },
+  {
+    "revision": "d888639decbe3d5c7dac",
+    "url": "/react_redux_no_middleware-basic-todo-list/static/css/main.9358471c.chunk.css"
+  },
+  {
+    "revision": "dc614831b2e2c7ad7169",
+    "url": "/react_redux_no_middleware-basic-todo-list/static/js/2.a5c58f4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_redux_no_middleware-basic-todo-list/static/js/2.a5c58f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d888639decbe3d5c7dac",
+    "url": "/react_redux_no_middleware-basic-todo-list/static/js/main.b7b0e9a1.chunk.js"
+  },
+  {
+    "revision": "1efcddc5b145841aacc9",
+    "url": "/react_redux_no_middleware-basic-todo-list/static/js/runtime-main.2b351901.js"
+  }
+]);
